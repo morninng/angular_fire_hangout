be8cd3fa8d68dbc70a7516d077b6938044487640
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-02-24 23:10:05 +0900

    first commi

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularFireHangoutApp
+ * @description
+ * # angularFireHangoutApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularFireHangoutApp', [
+    'ngAnimate'
+  ]);
