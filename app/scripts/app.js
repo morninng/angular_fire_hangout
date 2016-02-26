73c3e94b8430cc4116221a48daf2196d36388134
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-02-26 14:45:30 +0900

    initial game status is retrieved and go to ui-router status

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,15 +24,53 @@ angular.module('angularFireHangoutApp')
   	console.log("event id:" + MixideaSetting.event_id);
   	console.log("user id : " + MixideaSetting.own_user_id);
   	console.log("room type : " + MixideaSetting.room_type);
-	 $state.go('main.intro');
-	
+	 var event_id = MixideaSetting.event_id;
+	 var room_type = MixideaSetting.room_type;
+
+	var root_ref = new Firebase(MixideaSetting.firebase_url);
+	var game_status_ref = root_ref.child("event_related/game/" + event_id + "/game_status");
+	game_status_ref.once("value", function(snapshot){
+		var game_status = snapshot.val();
+		goto_state(room_type, game_status);
+	}, function(error_obj){
+		alert("event is corrupted, please confirm with mixidea administrator");
+	});
+
+	 
+
+	function goto_state(room_type, game_status){
+		switch(room_type){
+			case "main":
+				switch(game_status){
+					case "introduction":
+						$state.go('main.intro');
+					break;
+					case "preparation":
+						$state.go('main.preparation');
+					break;
+					case "debate":
+						$state.go('main.debate');
+					break;
+					case "reflection":
+						$state.go('main.reflection');
+					break;
+					case "complete":
+						$state.go('main.complete');
+					break;
+				}
+			break;
+
+			case "team_discussion":
+					$state.go('team_discussion');
+			break;
+		}
+	}
 
 }]);
  
 
 angular.module('angularFireHangoutApp')
   .config(['$stateProvider','MixideaSetting', function($stateProvider, MixideaSetting ) {
-
 
 	$stateProvider
 	.state('main', {
